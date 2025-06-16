1f25cf3f12a86bb06d4df1ad8de240065e845115
Ann3e <143268187+Ann3e@users.noreply.github.com>
2025-06-16 17:35:38 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://sahara-backend-6fn6.onrender.com',
   timeout: 80000,
   headers: {
     "Content-Type":"application/json",
